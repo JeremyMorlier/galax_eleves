910b607c814ece18f277fd27ff9c054e6188435c
MORLIER Jeremy <jeremy.morlier@imt-atlantique.net>
2023-03-10 14:22:06 +0100

    GPU version naive

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -5,15 +5,50 @@
 #define DIFF_T (0.1f)
 #define EPS (1.0f)
 
-__global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
+__global__ void compute_acc1(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+	float diffx, diffy, diffz;
+
+	for(int j = 0; j < n_particules; j++)
+	{
+		if(i != j)
+		{
+			diffx = positionsGPU[j].x - positionsGPU[i].x;
+			diffy = positionsGPU[j].y - positionsGPU[i].y;
+			diffz = positionsGPU[j].z - positionsGPU[i].z;
+
+			float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+			if (dij < 1.0)
+				{
+					dij = 10.0;
+				}
+				else
+				{
+					dij = std::sqrt(dij);
+					dij = 10.0 / (dij * dij * dij);
+				}
+
+			accelerationsGPU[i].x += diffx * dij * massesGPU[j];
+			accelerationsGPU[i].y += diffy * dij * massesGPU[j];
+			accelerationsGPU[i].z += diffz * dij * massesGPU[j];
+		}
+	}
+
 }
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
+	velocitiesGPU[i].x += accelerationsGPU[i].x * 2.0f;
+	velocitiesGPU[i].y += accelerationsGPU[i].x * 2.0f;
+	velocitiesGPU[i].z += accelerationsGPU[i].x * 2.0f;
+	positionsGPU[i].x += velocitiesGPU[i].x * 0.1f;
+	positionsGPU[i].y += velocitiesGPU[i].y * 0.1f;
+	positionsGPU[i].z += velocitiesGPU[i].z * 0.1f;
 }
 
 void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* accelerationsGPU, float* massesGPU, int n_particles)
